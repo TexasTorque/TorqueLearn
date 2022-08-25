134e69d8ee142b12852f4b77a58082a81f5eb9a4
Justus <jus@gtsbr.org>
2022-08-25 12:38:58 -0500

    Update back.js

--- layout/static/back.js
+++ layout/static/back.js
@@ -1,7 +1,9 @@
 // This is beautiful JavaScript
-function back() {
-  window.location.href =
-    window.location.pathname == "/Tutorials"
-      ? "/"
-      : window.location.pathname.split("/").slice(0, -1).join("/");
-}
+// No it's not
+// function back() {
+//   window.location.href =
+//     window.location.pathname == "/Tutorials"
+//       ? "/"
+//       : window.location.pathname.split("/").slice(0, -1).join("/");
+// }
+const back = () => window.location.href = window.location.pathname == "/Tutorials" ? "/" : window.location.pathname.split("/").slice(0, -1).join("/");
